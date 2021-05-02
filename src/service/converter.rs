4166df375f79c57e5c399537950f3e15c324ea68
ttzztztz <ttzztztz@outlook.com>
2021-05-02 19:22:33 +0800

    update: rule

--- src/service/converter.rs
+++ src/service/converter.rs
@@ -76,9 +76,6 @@ pub fn convert_website_object(
     }
 
     aggregate.sort_by(|lhs, rhs| {
-        if lhs.total_score == rhs.total_score && lhs.total_time == rhs.total_time {
-            return rhs.win_count.cmp(&lhs.win_count);
-        }
         if lhs.total_score == rhs.total_score {
             return lhs.total_time.cmp(&rhs.total_time);
         }
